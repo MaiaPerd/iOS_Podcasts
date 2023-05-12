755e3483c119c58cf6ff5a2aa665d8bdf858ee7d
maia <maia.perderizet@etudiant.univ-reims.fr>
2023-05-12 10:16:18 +0200

    first commit

---
+++ iOS_TP1_Podcast/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS_TP1_Podcast
+//
+//  Created by Perderizet Maïa on 11/05/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS_TP1_Podcast/iOS_TP1_PodcastApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_TP1_PodcastApp.swift
+//  iOS_TP1_Podcast
+//
+//  Created by Perderizet Maïa on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct iOS_TP1_PodcastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
